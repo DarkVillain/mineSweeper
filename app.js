0d09487985c5011c5e15cf0056bc5205b4f67064
Subhajit Sarkar <get2subhajit@hotmail.com>
2020-08-22 15:10:31 +0530

    Game Board has been created.

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector
+
+    //Create Board
+    function createBoard() {
+        for(let i = 0; i <= width*width) {
+            const square = document.createElement('div')
+            square.setAttribute('id', i)
+            grid.appendChild(square)
+            squares.push(square)
+        }
+    }
+    createBoard()
+
 })
