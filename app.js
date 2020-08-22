892462b751ac84ccb71a075c97f60ef1b6027e15
Subhajit Sarkar <get2subhajit@hotmail.com>
2020-08-22 15:19:36 +0530

    finally Game board is working

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector
+    const grid = document.querySelector('.grid')
     let width = 10
     let squares = []
 
     //Create Board
     function createBoard() {
-        for(let i = 0; i <= width*width) {
+        for(let i = 0; i < width*width; i++) {
             const square = document.createElement('div')
             square.setAttribute('id', i)
             grid.appendChild(square)
